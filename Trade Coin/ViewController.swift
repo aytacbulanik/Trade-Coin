80183a9ead2d4bdfabd51c1e81c02ee8bd236170
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-06 12:18:53 +0300

    design almost ready

--- Trade Coin/ViewController.swift
+++ Trade Coin/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monetCostLabel: UILabel!
+    @IBOutlet weak var moneyNameLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
